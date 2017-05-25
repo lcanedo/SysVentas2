adc85c55942a3eff2b36a569bf30e30ada839063
luis eduardo canedo torres <lcanedo12@gmail.com>
2017-05-24 23:37:10 -0500

    cracion de archivo global

---
+++ config/global.php
@@ -0,0 +1,20 @@
+<?php
+    //ip de la o servidor d ela base de datos
+    define("DB_HOST","localhost:90");
+
+    //nombre de la base de datos
+    define("DB_NAME","dbsistema");
+
+    //usuario d el abase de datos
+    define("DB_USERNAME","root");
+
+    //contrasena de la base de datos
+    define("DB_PASSWORD","root1234");
+
+    //codificacion de caracteres
+    define("DB_ENCODE","utf8");
+
+    //constante como nombre del proyecto
+    define("PRO_NOMBRE","INGVentas");
+    
+?>
