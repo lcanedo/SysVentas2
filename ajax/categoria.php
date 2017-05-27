0f1d191e9d6becca0de493f3bdbadda67e34533d
luis eduardo canedo torres <lcanedo12@gmail.com>
2017-05-26 23:00:11 -0500

    funciones insertar, editar, eliminar, lista y mostrar; envio mediante ajax

---
+++ ajax/categoria.php
@@ -0,0 +1,56 @@
+<?php
+require "../modelo/Categoria.php";
+
+$categoria = new Categoria();
+
+$idcategoria = isset($_POST["idcategoria"]) ? limpiarCadena($_POST["idcategoria"]) : "" ;
+$nombre = isset($_POST["nombre"]) ? limpiarCadena($_POST["nombre"]) : "" ;
+$descripcioin = isset($_POST["descripcion"]) ? limpiarCadena($_POST["descripcion"]) : "" ;
+
+switch ($_GET["op"]) {
+    case 'guardaryeditar':
+          if (empty($idcategoria)) {
+              $rspta = $categoria->insertar($nombre,$descripcioin);
+              echo $rspta ? "Categoria Registrada" : "Categoria no se pudo registrar";
+          }else {
+              $rspta = $categoria->editar($idcategoria,$nombre,$descripcioin);
+              echo $rspta ? "Categoria Actualizada" : "Categoria no se pudo Actualizar";
+          }
+        
+        break;
+    case 'desactivar':
+              $rspta = $categoria->desactivar($idcategoria);
+              echo $rspta ? "Categoria Desactivada" : "Categoria no se pudo Desactivar";
+        break;
+    case 'activar':
+              $rspta = $categoria->activar($idcategoria);
+              echo $rspta ? "Categoria Activada" : "Categoria no se pudo Activar";
+        break;
+    case 'mostrar':
+              $rspta = $categoria->mostrar($idcategoria);
+              //codificando el resultado utilizando json
+              echo json_encode($rspta);
+        break;
+    case 'listar':
+              $rspta = $categoria->listar();
+              //declaracion de un array
+              $data = Array();
+              while ($reg=$rspta->fetch_object()) {
+                  $data[]=array(
+                      "0"=>$reg->idcategoria,
+                      "1"=>$reg->nombre,
+                      "2"=>$reg->descripcion,
+                      "3"=>$reg->condicion
+                  );
+              }
+              $results = array(
+                  "sEcho"=>1,//informacion para el datatables
+                  "iTotalRecords"=>count($data),//enviamos el total de registros al datatable
+                  "iTotalDisplayRecords"=>count($data),//enviamos el total regisros a visualizar
+                  "aaData"=>$data
+              );
+              echo json_encode($results);
+        break;
+}
+
+?>
